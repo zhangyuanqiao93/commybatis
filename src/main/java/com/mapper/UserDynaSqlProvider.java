42896df647b9686d7d3ffddab832142327b11f2e
zhangyuanqiao93 <zhangyuanqiao0912@163.com>
2018-06-11 16:02:48 +0800

    1.增加日志

--- src/main/java/com/mapper/UserDynaSqlProvider.java
+++ src/main/java/com/mapper/UserDynaSqlProvider.java
@@ -1,11 +1,20 @@
 package com.mapper;
 
 import org.apache.ibatis.jdbc.SQL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class UserDynaSqlProvider {
+    //mybatis日志
+    private static final Logger log = LoggerFactory.getLogger(UserDynaSqlProvider.class);
 
+    /**
+     * 基于动态SQL的CRUD
+     * @param param map对象
+     * @return tb_user查询结果
+     */
     public String selectWithParam(final Map<String,Object> param){
         return new SQL(){
             {
